10c01ebd6b522201e9aa326316f32780939a3f1c
Boogertastic <31267041+Boogertastic@users.noreply.github.com>
2018-12-19 22:47:02 +0800

    sorry again

--- dmcspy/ode/rk/methods.py
+++ dmcspy/ode/rk/methods.py
@@ -21,40 +21,39 @@ Output:
 
 def rk2(T, N, function, init):
         start = time()
-	grid = np.linspace(0, T, N+1) #generates equally segmented values of t
-	h = grid[1] - grid[0]
-	size = len(init)#init.shape[0]
-	#pre-allocation
-	x = np.zeros((size, len(grid))) 
-	x[:,0] = init
-	print(x)
-	for k in range(N):
-		w1 = x[:, k]
-		w2 = w1 + 0.5*h*f(w1)
-		x[:, k+1] = x[:,k] + (h/4)*(2*f(w1) + 2*f(w2))
-	end = time()
-	t = end - start
-	return x, t
+        grid = np.linspace(0, T, N+1) #generates equally segmented values of t
+        h = grid[1] - grid[0]
+        size = len(init)#init.shape[0]
+        #pre-allocation
+        x = np.zeros((size, len(grid))) 
+        x[:,0] = init
+        print(x)
+        for k in range(N):
+        	w1 = x[:, k]
+        	w2 = w1 + 0.5*h*f(w1)
+        	x[:, k+1] = x[:,k] + (h/4)*(2*f(w1) + 2*f(w2))
+        end = time()
+        t = end - start
+        return x, t
 
 def rk4(T, N, function, init):
         start = time()
-	grid = np.linspace(0, T, N+1) #generates equally segmented values of t
-	h = grid[1] - grid[0]
-	size = len(init)#init.shape[0]
+        grid = np.linspace(0, T, N+1) #generates equally segmented values of t
+        h = grid[1] - grid[0]
+        size = len(init)#init.shape[0]
 	#pre-allocation
-	x = np.zeros((size, len(grid)))
-	print('row size is = ')
-	print(size)
-	print('comlumn size is =')
-	print(len(grid))
-	x[:,0] = init
-	for k in range(N):
-		w1 = x[:, k]
-		w2 = w1 + 0.5*h*f(w1)
-		w3 = w1 + 0.5*h*f(w2)
-		w4 = w1 + h*f(w3)
-		x[:, k+1] = x[:,k] + (h/6)*(f(w1) + 2*f(w2) + 2*f(w3) + f(w4))
-	end = time()
-	t = end - start
-	return x, t
-
+        x = np.zeros((size, len(grid)))
+        print('row size is = ')
+        print(size)
+        print('comlumn size is =')
+        print(len(grid))
+        x[:,0] = init
+        for k in range(N):
+        	w1 = x[:, k]
+        	w2 = w1 + 0.5*h*f(w1)
+        	w3 = w1 + 0.5*h*f(w2)
+        	w4 = w1 + h*f(w3)
+        	x[:, k+1] = x[:,k] + (h/6)*(f(w1) + 2*f(w2) + 2*f(w3) + f(w4))
+        end = time()
+        t = end - start
+        return x, t
