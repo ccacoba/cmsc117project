7454f723657968a83a30f2244d197b926174ffcd
Boogertastic <31267041+Boogertastic@users.noreply.github.com>
2018-12-19 22:44:43 +0800

    haha sorry

--- dmcspy/ode/base.py
+++ dmcspy/ode/base.py
@@ -2,7 +2,6 @@ from .euler import *
 from .adams_bashfort import *
 from .rk import *
 from matplotlib import pyplot as plt
-def PrintResults(x_val, time_val, x=True, time=True):
 """
 This method displays the graph the solutions of the First order Differential
 equations by the different methods for ODEs
@@ -11,11 +10,12 @@ arguments:
         time     -    boolean value to print the time elapsed(in ms)
         x        -    displays the graph of the chosen method that's used
 """
-	if x:
-                    plt.plot(x_val[0,:], x_val[1,:],'bs', ls = '-.', ms = '1.0')
-                    plt.xlabel('t')
-                    plt.ylabel('x(t)')
-                    plt.show()
-	if time:
-		print("Time elapsed: ", end='')
-		print(time_val)
+def PrintResults(x_val, time_val, x=True, time=True):
+        if x:
+                plt.plot(x_val[0,:], x_val[1,:],'bs', ls = '-.', ms = '1.0')
+                plt.xlabel('t')
+                plt.ylabel('x(t)')
+                plt.show()
+        if time:
+                print("Time elapsed: ", end='')
+                print(time_val)
